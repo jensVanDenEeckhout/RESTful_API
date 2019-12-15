568ec67edc92829be50dc38fcd2013fd5561a30a
Jens Van den Eeckhout <jens.vandeneeckhout@student.odisee.be>
2019-12-15 10:59:08 +0100

    Revert "revert fullstack laravel api and vue frontend  added delete article"
    This reverts commit 9b05b257ad7bb2b85550b1b6ad9b1ba5aef6c4b6.

--- larticles/resources/js/app.js
+++ larticles/resources/js/app.js
@@ -1,17 +1,1 @@
 require('./bootstrap');
-
-
-window.Vue = require('vue');
-
-
-
-import Articles from './components/Articles.vue';
-import Navbar from './components/Navbar.vue';
-
- //'articles', require('./components/Articles.vue') // not working
- Vue.component('articles', Articles);
- Vue.component('navbar', Navbar);
-
-const app = new Vue({
-    el: '#app'
-});
